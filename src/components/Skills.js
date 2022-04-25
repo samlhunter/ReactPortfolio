ef9b6d845bc753f539f97dac0ad93d4314e50ceb
samlhunter <samhunter98@gmail.com>
2022-04-25 10:06:54 -0600

    skills component complete

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,9 +14,8 @@ export default function Skills() {
                 Skills &amp; Technologies
             </h1>
             <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-                ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-                possimus est.
+                Here are some of the skills that I feel make me a valuable asset. I take pride in diversifying my knowledge and love to learn new skills. I have also learned some of
+                these skills on my own.
             </p>
             </div>
             <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
         title: "GC Home Care",
         subtitle: "Flutter",
         description:
-            "Commissioned by a custom home building company, this is an application built using a flutter framework that is a customer care program",
+            "Commissioned by a custom home building company, this is an application built using a flutter framework that is a customer care program. The goal is to allow clients to submit repair and maintenance requests with no hassle.",
         image: "./images/flutter.jpg",
         link: "https://github.com/samlhunter/GCHomeCare",
     },
@@ -13,7 +13,7 @@ export const projects = [
         title: "Tweeter",
         subtitle: "Java and AWS",
         description:
-            "My software engineering class to build a clone of twitter. Implementing model-view-presenter architecture, and using AWS services - lambda, dynamodb, SQS",
+            "In my software engineering class, I built a clone of twitter. Implementing model-view-presenter architecture, and using AWS services - lambda, dynamodb, SQS, API Gateway. I used lots of software design principles in this project.",
         image: "./images/tweeter.png",
         link: "https://github.com/samlhunter/CS340-Tweeter",
     },
@@ -27,7 +27,7 @@ export const projects = [
     },
     {
         title: "Backpropogation Model",
-        subtitle: "Python Jupyter Notebooks",
+        subtitle: "Python and Jupyter Notebooks",
         description:
             "Using a jupyter notebook, I created my own version of a backpropogation model with tests that compare performance to the popular scikit learn, this is only configured to use one hidden layer of nodes ",
         image: "./images/backprop.jpg",
@@ -35,5 +35,5 @@ export const projects = [
     },
 ];
 
-export const skills = ["Git", "Node", "Python"];
+export const skills = ["Python", "Node.js", "Java", "ETL's", "SQL", "Git", "C++", "C", "AWS", "React", "Flutter"];
 
