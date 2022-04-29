68a63dcfcbbb5dd10ef4c9861a5e09b45e1a2fb8
samlhunter <samhunter98@gmail.com>
2022-04-28 21:40:06 -0600

    ready for new updates

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default function About() {
                     <a
                         href="#contact"
                         className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                        Work With Me
+                        Reach Out To Me
                     </a>
                     <a
                         href="#projects"
