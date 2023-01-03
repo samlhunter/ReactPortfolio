ac0dd2a869320bfe720ca3c9a5a139b73b4b8dc8
Samuel Hunter <samhunter98@gmail.com>
2023-01-03 15:49:37 -0700

    testing for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ export default function Navbar() {
         <header className="bg-gray-800 md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <div className="title-font font-medium text-white mb-4 md:mb-0">
-                    <Link to={'/'} className="ml-3 text-xl">
+                    <Link to={'/'} className="ml-3 text-3xl md:text-xl">
                         Sam Hunter
                     </Link>
                 </div>
-                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
                     <Link className="mr-5 hover:text-white" to={'/about'}>
                         About
                     </Link>
@@ -30,7 +30,7 @@ export default function Navbar() {
                 <div className="flex items-center align-middle">
                     <a 
                         href="https://github.com/samlhunter" 
-                        className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0"
+                        className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded mt-4 md:mt-0"
                     >
                         <img
                             className="object-cover object-center rounded"
@@ -41,7 +41,7 @@ export default function Navbar() {
                     </a>
                     <a 
                         href="https://www.linkedin.com/in/samuellhunter" 
-                        className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0"
+                        className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded mt-4 md:mt-0"
                     >
                         <img
                             className="object-cover object-center rounded"
--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -5,22 +5,22 @@ import Navbar from "../components/Navbar";
 
 export default function Skills() {
     return (
-        <div className="text-gray-400 bg-gray-900">
+        <div className="flex flex-col h-screen text-gray-400 bg-gray-900 overflow-auto">
             <Navbar />
-            <div className="container px-5 py-5 mx-auto">
+            <div className="flex flex-col grow container px-5 py-5 mx-auto">
                 <div className="text-center mb-5">
-                    <ChipIcon className="w-10 inline-block mb-4" />
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                    <ChipIcon className="w-20 md:w-12 inline-block mb-4" />
+                    <h1 className="text-5xl md:text-3xl font-medium title-font text-white mb-4">
                         Skills &amp; Technologies
                     </h1>
-                    <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                    <p className="text-base md:text-lg leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
                         Here are some of the skills that I feel make me a valuable asset. I take pride in diversifying my knowledge and love to learn new skills. I have also learned some of
                         these skills on my own.
                     </p>
                 </div>
                 <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
                     {skills.map((skill) => (
-                        <div key={skill} className="p-2 sm:w-1/2 w-full">
+                        <div key={skill} className="p-2 w-full md:w-1/2">
                             <div className="bg-gray-800 rounded flex p-4 h-full items-center">
                                 <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
                                 <span className="title-font font-medium text-white">
